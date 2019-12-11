9150ccf29a8d8762ef1493be829be9789f8ff3f9
Chris Buonocore <chrisdistrict@gmail.com>
2019-12-11 11:56:46 -0500

    Multiple results (#1)
    * multiple results
    
    * add readme
    
    * multiple result styling

--- client/src/App.js
+++ client/src/App.js
@@ -3,24 +3,35 @@ import { Button, Typography, Grid, TextField } from "@material-ui/core";
 import { ThemeProvider } from "@material-ui/styles";
 
 import MyContract from "./contracts/MyContract.json";
+import ReactTooltip from 'react-tooltip'
+
 import getWeb3 from "./utils/getWeb3";
 
 import { theme } from "./utils/theme";
+import logo from './assets/logo.png'
 import Header from "./components/Header";
+
 import "./App.css";
 
 const GAS = 500000;
 const GAS_PRICE = "20000000000";
+const REQUEST_INFO_TEXT = `This will tie 4 location transactions for today's forecast to the Contract Surfer blockchain`
+
+const getFeetFromResult = (result) => Number(result.slice(6, 8)) / 10
+
+const INITIAL_LOCATION_STATE = {
+  location1: "33.878727, -118.427179",
+  location2: "33.148605, -117.353412",
+  location3: "33.878727, -118.427179",
+  location4: "33.148605, -117.353412"
+}
 
 class App extends Component {
   state = {
     web3: null,
     accounts: null,
+    ...INITIAL_LOCATION_STATE,
     contract: null,
-    location1: "33.878727, -118.427179",
-    location2: "33.148605, -117.353412",
-    location3: "33.878727, -118.427179",
-    location4: "33.148605, -117.353412",
     resultReceived: false,
     result: "0"
   };
@@ -52,7 +63,7 @@ class App extends Component {
       const component = this;
       async function loopRefresh() {
         await component.refreshState();
-        setTimeout(loopRefresh, 1000);
+        setTimeout(loopRefresh, 2000);
       }
       loopRefresh();
     } catch (error) {
@@ -63,14 +74,23 @@ class App extends Component {
     }
   };
 
+  getLocations = () => {
+    const {location1, location2, location3, location4} = this.state
+    return [location1, location2, location3, location4]
+  }
+
   refreshState = async () => {
-    const resultReceived = await this.state.contract.methods
-      .resultReceived()
-      .call();
-    const result = (
-      await this.state.contract.methods.result().call()
-    ).toString();
+    const resultReceived = {}
+    const result = {}
+    const locations = this.getLocations()
+    for (var i = 0; i < locations.length; i ++) {
+      const received_i = await this.state.contract.methods.getResultReceived(i).call();
+      const result_i = (await this.state.contract.methods.getResult(i).call()).toString();
+      resultReceived[i] = received_i
+      result[i] = result_i
+    }
     this.setState({ resultReceived, result });
+    // console.group('refresh', resultReceived, result)
   };
 
   handleUpdateForm = (name, value) => {
@@ -78,16 +98,22 @@ class App extends Component {
   };
 
   handleRequestResult = async () => {
-    await this.state.contract.methods
-      .makeRequest(this.state.location1.toString())
-      .send({ from: this.state.accounts[0], gas: GAS, gasPrice: GAS_PRICE });
+    const locations = this.getLocations()
+    // TODO: group into one transaction or make parallel.
+    for (var i = 0; i < locations.length; i ++) { 
+      console.log('requesting', locations[i], i)
+      const requestId = await this.state.contract.methods.makeRequest(locations[i].toString(), i).send({ from: this.state.accounts[0], gas: GAS, gasPrice: GAS_PRICE });
+      console.log('requested', requestId)
+    }
   };
 
   handleResetResult = async () => {
-    await this.state.contract.methods
-      .resetResult()
-      .send({ from: this.state.accounts[0], gas: GAS, gasPrice: GAS_PRICE });
+    this.setState({...INITIAL_LOCATION_STATE})
+    // await this.state.contract.methods
+    //   .resetResult()
+    //   .send({ from: this.state.accounts[0], gas: GAS, gasPrice: GAS_PRICE });
   };
+
 
   render() {
     if (!this.state.web3) {
@@ -101,11 +127,25 @@ class App extends Component {
         </ThemeProvider>
       );
     }
+
+    const {result, resultReceived} = this.state
+    const locations = this.getLocations()
+    const parsedResults = []
+    locations.map((loc, i) => {
+      if (result[i]) {
+        parsedResults.push(parseFloat(getFeetFromResult(result[i])))
+      }
+    })
+    const hasAllResults = Object.values(resultReceived || {}).every(x => x)
+    console.log('parsed', parsedResults)
+    const bestResult = Math.max(...parsedResults)
     return (
       <ThemeProvider theme={theme}>
+        <ReactTooltip />
         <div className="App">
           <Header />
 
+          <img src={logo} className='header-logo'/>
           <Typography variant="h5" style={{ marginTop: 32 }}>
             {`Please enter the surf venue locations you would like to select from`}
           </Typography>
@@ -181,14 +221,21 @@ class App extends Component {
               />
             </Grid>
           </Grid>
-
+            
           <Typography variant="h5" style={{ marginTop: 32 }}>
-            {`Result received: ${this.state.resultReceived}`}
+            {`Results ready: ${JSON.stringify(resultReceived)}`}
           </Typography>
 
           <Typography variant="h5" style={{ marginTop: 32 }}>
-            {`Result: ${Number(this.state.result.slice(6, 8)) / 10} Feet`}
+            {`Results: ${JSON.stringify(parsedResults)} Feet`}
           </Typography>
+
+          {hasAllResults && <div>
+            <Typography variant="h5" style={{ marginTop: 32 }}>
+                Best Result: <b>{`${bestResult} Feet at Location ${parsedResults.indexOf(bestResult) + 1}`}</b>
+            </Typography>
+            <p className='success-text'>Looks like some good waves ahead.</p>
+          </div>}
 
           <Grid container style={{ marginTop: 32 }}>
             <Grid item xs>
@@ -197,11 +244,13 @@ class App extends Component {
                 color="primary"
                 onClick={() => this.handleResetResult()}
               >
-                Reset Result
+                Reset Inputs
               </Button>
             </Grid>
             <Grid item xs>
               <Button
+                data-place='top'
+                data-tip={REQUEST_INFO_TEXT}
                 variant="contained"
                 color="primary"
                 onClick={() => this.handleRequestResult()}
--- test.js
+++ test.js
@@ -10,8 +10,8 @@ module.exports = async function() {
   console.log(`Initial result: ${result.toString()}`);
 
   console.log("Making a Chainlink request using a Honeycomb job...");
-  requestId = await myContract.makeRequest.call("33.878727, -118.427179");
-  await myContract.makeRequest("33.878727, -118.427179");
+  requestId = await myContract.makeRequest.call("33.878727, -118.427179", 0);
+  await myContract.makeRequest("33.878727, -118.427179", 0);
   console.log(`Request ID: ${requestId}`);
 
   console.log("Waiting for the request to be fulfilled...");
